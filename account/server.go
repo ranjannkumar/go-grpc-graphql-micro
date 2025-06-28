42438c04aa3c6606ea6ddc6e20ae66ecf2dbfd34
Ranjan kumar <ranjankumar5673@gmail.com>
2025-06-28 23:30:41 +0530

    Implement gRPC server handlers and runtime

--- account/server.go
+++ account/server.go
@@ -1,32 +1,66 @@
 package account
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"net"
+	"github.com/ranjannkumar/go-grpc-grpahql-microservice/account/pb/github.com/ranjannkumar/go-grpc-grpahql-microservice/account/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
 
 type grpcServer struct {
 	service Service
 }
 
-func (s *grpcServer) PostAccount(ctx context.Context,r *pb.)(*pb.,error){
+func ListenGRPC(s Service,port int)error{
+	lis,err := net.Listen("tcp",fmt.Sprintf(":%d",port))
+	if err !=nil{
+		return err
+	}
+	serv := grpc.NewServer()
+	pb.RegisterAccountServiceServer(serv,&grpcServer{s})
+	reflection.Register(serv)
+	return serv.Serve(lis)
+}
+
+func (s *grpcServer) PostAccount(ctx context.Context,r *pb.PostAccountRequest)(*pb.PostAccountResponse,error){
 	a,err := s.service.PostAccount(ctx,r.Name)
 	if err !=nil{
 		return nil,err
 	}
-	return &pb.{}
+	return &pb.PostAccountResponse{Account:&pb.Account{
+		Id:a.ID,
+		Name:a.Name,
+	}},nil
 }
 
-func(s *grpcServer)GetAccount(ctx context.Context,r *pb.)(*pb.,error){
+func(s *grpcServer)GetAccount(ctx context.Context,r *pb.GetAccountRequest)(*pb.GetAccountResponse,error){
 	a,err := s.service.GetAccount(ctx,r.Id)
 	if err !=nil{
 		return nil,err
 	}
-	return &pb.{}
+	return &pb.GetAccountResponse{
+		Account: &pb.Account{
+			Id: a.ID,
+			Name: a.Name,
+		},
+	},nil
 }
 
-func(s *grpcServer)GetAccounts(ctx context.Context,r *pb.)(*pb.,error){
-	a,err := s.service.GetAccounts(ctx,r.Id)
+func(s *grpcServer)GetAccounts(ctx context.Context,r *pb.GetAccountsRequest)(*pb.GetAccountsResponse,error){
+	res,err := s.service.GetAccounts(ctx,r.Id)
 	if err !=nil{
 		return nil,err
 	}
-	return &pb.{}
+	accounts := []*pb.Account{}
+	for_,p := range res {
+		accounts = append(accounts,&pb.Account{
+			Id: p.ID,
+			Name: p.Name,
+		 },
+	  )
+	}
+	return &pb.GetAccountsResponse{Accounts:accounts},nil
 }
 
