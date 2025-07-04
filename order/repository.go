372f5c3fea082fb53f0b66aeb571a683c6e0314c
Ranjan kumar <ranjankumar5673@gmail.com>
2025-07-04 23:26:49 +0530

    feat(main): Implement order service bootstrap and configuration and fix error in repo.go

--- order/repository.go
+++ order/repository.go
@@ -3,51 +3,54 @@ package order
 import (
 	"context"
 	"database/sql"
-
 	"github.com/lib/pq"
 )
 
 type Repository interface {
 	Close()
-	PutOrder(ctx context.Context,o Order)error
-	GetOrdersForAccount(ctx context.Context,accountID string)([]Order,error)
+	PutOrder(ctx context.Context, o Order) error
+	GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error)
 }
 
-type postgresRepository struct{
+type postgresRepository struct {
 	db *sql.DB
 }
 
-func NewPostgresRepository(url string)(Repository,error){
-	db,err := sql.Open("postgres",url)
-	if err !=nil{
-		return nil,err
+func NewPostgresRepository(url string) (Repository, error) {
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		return nil, err
 	}
 	err = db.Ping()
-	if err != nil{
-		return nil,err
+	if err != nil {
+		return nil, err
 	}
-	return  &postgresRepository{db},nil
+	return &postgresRepository{db}, nil
 }
 
-func (r *postgresRepository) Close(){
+func (r *postgresRepository) Close() {
 	r.db.Close()
 }
 
-func (r *postgresRepository) PutOrder(ctx context.Context,o Order)( err error){
-	tx,err := r.db.BeginTx(ctx,nil)
-	if err != nil{
+func (r *postgresRepository) PutOrder(ctx context.Context, o Order) (err error) {
+	tx, err := r.db.BeginTx(ctx, nil)
+	if err != nil {
 		return err
 	}
-	defer func ()  {
-		if err != nil{
+				// The defer block ensures that the transaction is either committed or rolled back 
+			// based on whether an error occurred. If err is not nil when the function exits, tx.Rollback() is called. Otherwise, tx.Commit() is called
+	defer func() {
+		if err != nil {
 			tx.Rollback()
 			return
 		}
 		err = tx.Commit()
 	}()
-	tx.ExecContext(
+
+	// Insert order
+	_, err = tx.ExecContext(
 		ctx,
-		"INSERT INTO orders(id,created_at,account_id,total_price) VALUES ($1,$2,$3,$4)",
+		"INSERT INTO orders(id, created_at, account_id, total_price) VALUES($1, $2, $3, $4)",
 		o.ID,
 		o.CreatedAt,
 		o.AccountID,
@@ -57,47 +60,51 @@ func (r *postgresRepository) PutOrder(ctx context.Context,o Order)( err error){
 		return
 	}
 
-	stmt, _ := tx.PrepareContext(ctx,pq.CopyIn("order_products","order_id","product_id","quantity"))
-	for _,p := range o.Products{
-		_,err = stmt.ExecContext(ctx,o.ID,p.ID,p.Quantity)
-		if err != nil{
+	// Insert order products
+	stmt, _ := tx.PrepareContext(ctx, pq.CopyIn("order_products", "order_id", "product_id", "quantity"))
+	for _, p := range o.Products {
+		_, err = stmt.ExecContext(ctx, o.ID, p.ID, p.Quantity)
+		if err != nil {
 			return
 		}
 	}
-	_,err = stmt.ExecContext(ctx)
-	if err !=nil{
+	_, err = stmt.ExecContext(ctx)
+	if err != nil {
 		return
 	}
 	stmt.Close()
-	return
 
+	return
 }
 
-func (r *postgresRepository) GetOrdersForAccount(ctx context.Context,accountID string)([]Order,error){
-	rows,err := r.db.QueryContext(
+func (r *postgresRepository) GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error) {
+	rows, err := r.db.QueryContext(
 		ctx,
 		`SELECT
-		o.id,
-		o.created_at,
-		o.total_price::money::numeric::float8,
-		op.product_id,
-		op.quantity
-		FROM orders o JOIN order_products op ON(o.id=op.order_id)
-		WHERE o.account_id = $1,
-		ORDER BY o.id
-		`,
+      o.id,
+      o.created_at,
+      o.account_id,
+      o.total_price::money::numeric::float8,
+      op.product_id,
+      op.quantity
+    FROM orders o JOIN order_products op ON (o.id = op.order_id)
+    WHERE o.account_id = $1
+    ORDER BY o.id`,
 		accountID,
 	)
-	if err !=nil{
-		return nil,err
+	if err != nil {
+		return nil, err
 	}
 	defer rows.Close()
+
 	orders := []Order{}
+	order := &Order{}
 	lastOrder := &Order{}
-	OrderedProduct := &OrderedProduct{}
+	orderedProduct := &OrderedProduct{}
 	products := []OrderedProduct{}
 
-	for rows.Next(){
+	// Scan rows into Order structs
+	for rows.Next() {
 		if err = rows.Scan(
 			&order.ID,
 			&order.CreatedAt,
@@ -105,38 +112,45 @@ func (r *postgresRepository) GetOrdersForAccount(ctx context.Context,accountID s
 			&order.TotalPrice,
 			&orderedProduct.ID,
 			&orderedProduct.Quantity,
-		);err != nil{
-			return nil,err
+		); err != nil {
+			return nil, err
 		}
-		if lastOrder.ID !="" && lastOrder.ID != order.ID{
+		// Scan order
+		if lastOrder.ID != "" && lastOrder.ID != order.ID {
 			newOrder := Order{
-					ID:         lastOrder.ID,
-					AccountID:  lastOrder.AccountID,
-					CreatedAt:  lastOrder.CreatedAt,
-					TotalPrice: lastOrder.TotalPrice,
-					Products:   lastOrder.Products,
-      }
+				ID:         lastOrder.ID,
+				AccountID:  lastOrder.AccountID,
+				CreatedAt:  lastOrder.CreatedAt,
+				TotalPrice: lastOrder.TotalPrice,
+				Products:   products,
+			}
 			orders = append(orders, newOrder)
 			products = []OrderedProduct{}
 		}
+		// Scan products
 		products = append(products, OrderedProduct{
 			ID:       orderedProduct.ID,
 			Quantity: orderedProduct.Quantity,
 		})
-		*lastOrder = *&order
+
+		*lastOrder = *order
 	}
-	if lastOrder != nil{
+
+	// Add last order (or first :D)
+	if lastOrder != nil {
 		newOrder := Order{
-					ID:         lastOrder.ID,
-					AccountID:  lastOrder.AccountID,
-					CreatedAt:  lastOrder.CreatedAt,
-					TotalPrice: lastOrder.TotalPrice,
-					Products:   lastOrder.Products,
-      }
-			orders = append(orders, newOrder)
+			ID:         lastOrder.ID,
+			AccountID:  lastOrder.AccountID,
+			CreatedAt:  lastOrder.CreatedAt,
+			TotalPrice: lastOrder.TotalPrice,
+			Products:   products,
+		}
+		orders = append(orders, newOrder)
 	}
-	if err = rows.Err();err!=nil{
-		return nil,err
+
+	if err = rows.Err(); err != nil {
+		return nil, err
 	}
-	return orders,nil
+
+	return orders, nil
 }
